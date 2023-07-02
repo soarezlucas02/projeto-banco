d63b5f4f74a2c11aac8538257c7f8fe73a90bcc3
soarezlucas02 <lucassoaresesouza@gmail.com>
2023-07-01 22:40:40 -0300

    definindo classe ContaCorrente e funcao sacar

--- index.js
+++ index.js
@@ -1,22 +1,44 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
-    saldo
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Lucas";
 cliente1.cpf = 121184145;
-cliente1.agencia = 1001;
-cliente1.saldo= 0;
 
+
+const cliente2 = new Cliente();
 cliente2.nome = "Leticia";
 cliente2.cpf = 121184142;
-cliente2.agencia = 1001;
-cliente2.saldo= 0;
+
+
+const contaCorrenteLucas= new ContaCorrente();
+contaCorrenteLucas.agencia = 1001;
+contaCorrenteLucas.saldo= 0;
+
+
+
+const contaCorrenteLeticia= new ContaCorrente();
+contaCorrenteLeticia.agencia = 1001;
+contaCorrenteLeticia.saldo= 0;
+
+
+console.log(contaCorrenteLucas);
+contaCorrenteLucas.saldo=100;
+contaCorrenteLucas.sacar(50);
+console.log(contaCorrenteLucas); 
 
 console.log(cliente1);
 console.log(cliente2);
