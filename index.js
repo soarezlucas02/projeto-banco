52bfdf31f829ab6e4075fedc9c8f5b3b6b15a598
soarezlucas02 <lucassoaresesouza@gmail.com>
2023-07-01 21:49:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+
+cliente1.nome = "Lucas";
+cliente1.cpf = 121184145;
+cliente1.agencia = 1001;
+cliente1.saldo= 0;
+
+cliente2.nome = "Leticia";
+cliente2.cpf = 121184142;
+cliente2.agencia = 1001;
+cliente2.saldo= 0;
+
+console.log(cliente1);
+console.log(cliente2);
